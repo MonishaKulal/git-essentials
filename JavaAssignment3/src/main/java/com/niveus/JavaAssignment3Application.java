9f16648ac1547deb007abde856f338274f987e5a
MonishaKulal <monisha.vkb@gmail.com>
2023-08-23 11:20:40 +0530

    New Assignment3 files

---
+++ JavaAssignment3/src/main/java/com/niveus/JavaAssignment3Application.java
@@ -0,0 +1,13 @@
+package com.niveus;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAssignment3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAssignment3Application.class, args);
+		System.out.println("Application Started.........");
+	}
+
+}
---
+++ JavaAssignment3/src/test/java/com/niveus/JavaAssignment3ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.niveus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class JavaAssignment3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
